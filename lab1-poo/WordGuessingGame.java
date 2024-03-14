a32e5f40316ca13e0dfe658c4c3ee751476964ab
1kayzar4 <tdtrabalho14@gmail.com>
2024-03-14 10:32:36 +0000

    teste

---
+++ lab1-poo/WordGuessingGame.java
@@ -0,0 +1,20 @@
+
+/**
+ * Escreva uma descrição da classe WordGuessingGame aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class WordGuessingGame
+{
+    private String hiddenWord;
+    private String guessedWord;
+    private int nunberOfTries;
+    
+    public WordGuessingGame(){
+        hiddenWord="abc";
+        guessedWord="___";
+    }
+    
+    
+}
