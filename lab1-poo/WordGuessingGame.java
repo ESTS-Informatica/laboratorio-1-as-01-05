416517eec6d151c2cb57831557da0ae903bd20b8
1kayzar4 <tdtrabalho14@gmail.com>
2024-03-14 10:46:41 +0000

    nivel 1

--- lab1-poo/WordGuessingGame.java
+++ lab1-poo/WordGuessingGame.java
@@ -14,7 +14,34 @@ public class WordGuessingGame
     public WordGuessingGame(){
         hiddenWord="abc";
         guessedWord="___";
+        
     }
     
+    public String getHiddenWord(){
+        return hiddenWord;
+    }
     
+    public String getguessedWord(){
+        return guessedWord;
+    }
+    
+    public int getnunberOfTries(){
+        return nunberOfTries;
+    }
+    
+    public void setnunberOfTries(int nunberOfTries){
+        this.nunberOfTries=nunberOfTries;
+    }
+    
+     public void sethiddenWord(String hiddenWord){
+        this.hiddenWord=hiddenWord;
+    }
+    
+     public void setguessedWord(String guessedWord){
+        this.guessedWord=guessedWord;
+    }
+    
+    public void showGuessedWords(){
+    System.out.println("palavra advinhada: " + guessedWord);
+    }
 }
